8df79a032bb27af17673024b1682d7a9ebec1d3a
jangirsamarth <samarth8947@gmail.com>
2025-03-16 23:58:17 +0530

    changed insta handel

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -750,7 +750,7 @@ export function Home() {
       <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end space-y-4">
         {/* Instagram Button */}
         <motion.a
-          href="https://www.instagram.com/yourusername" // Replace with your Instagram URL
+          href="https://www.instagram.com/revnroar.ig/" // Replace with your Instagram URL
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center justify-center w-16 h-16 bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-600 rounded-full shadow-lg hover:opacity-90 transition-colors"
--- client/src/pages/ItineraryPage.jsx
+++ client/src/pages/ItineraryPage.jsx
@@ -225,7 +225,7 @@ const ItineraryPage = () => {
       <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end space-y-4">
         {/* Instagram Button */}
         <motion.a
-          href="https://www.instagram.com/yourusername"
+          href="https://www.instagram.com/revnroar.ig/"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center justify-center w-16 h-16 bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-600 rounded-full shadow-lg hover:opacity-90 transition-colors"
